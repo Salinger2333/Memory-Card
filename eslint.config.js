ab0bffc08578f98c2c7e05913b17922e7e1a97b8
Salinger2333 <1565664814@qq.com>
2026-01-16 19:29:00 +0800

    chore: add node environment

--- eslint.config.js
+++ eslint.config.js
@@ -26,4 +26,16 @@ export default defineConfig([
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
+  {
+    files:['api/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node, 
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+
+    },
+  }
 ])
